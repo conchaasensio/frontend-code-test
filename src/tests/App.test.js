e39392599c0825f0e3f10f0659aec38d3d48b6cd
Concha Asensio <conchaasensiomr@gmail.com>
2024-11-10 10:12:30 +0100

    Test: can add a box

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,15 @@ import { fireEvent, render } from '@testing-library/react';
 import App from '../components/App';
 import { MainStore } from '../stores/MainStore';
 
+test('can add a box', () => {
+  const store = MainStore.create();
+  const { getByTestId } = render(<App store={store} />);
+
+  fireEvent.click(getByTestId('add-button'));
+
+  expect(store.boxes).toHaveLength(1);
+});
+
 test('can select a box', () => {
   const store = MainStore.create();
   const box = store.createBox();
